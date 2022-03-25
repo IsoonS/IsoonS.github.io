bb5425199ea79b785b4d3587dec2117508eacc13
Chotipat <chotipat.s@kkumail.com>
2022-03-25 11:16:59 +0700

    Change stopwatch color and Add stopwatch script

--- app.js
+++ app.js
@@ -405,5 +405,47 @@ function selectSport() {
 
 
 
+// https://dev.to/stackfindover/how-to-create-a-stopwatch-in-javascript-57a8
+// Stopwatch script
+const watch = document.querySelector("#stopwatch");
+      let millisecound = 0;
+      let timer;
+
+      function timeStart(){
+        watch.style.color = "#002581";
+        clearInterval(timer);
+        timer = setInterval(() => {
+          millisecound += 10;
+
+          let dateTimer = new Date(millisecound);
+
+          watch.innerHTML = 
+          ('0'+dateTimer.getUTCHours()).slice(-2) + ':' +
+          ('0'+dateTimer.getUTCMinutes()).slice(-2) + ':' +
+          ('0'+dateTimer.getUTCSeconds()).slice(-2) + ':' +
+          ('0'+dateTimer.getUTCMilliseconds()).slice(-3,-1);
+        }, 10);
+      }
+
+
+      function timePaused() {
+        watch.style.color = "red";
+        clearInterval(timer);
+      }
+
+      function timeReset(){
+        watch.style.color = "#002581";
+        setInterval(timer)
+        millisecound = 0;
+        watch.innerHTML = "00:00:00:00";
+      }
+
+      document.addEventListener('click', (e) => {
+        const el = e.target;
+
+        if(el.id === 'start') timeStart();
+        if(el.id === 'pause') timePaused();
+        if(el.id === 'reset') timeReset();
+      })
 
 

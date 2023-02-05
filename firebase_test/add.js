93353968531a69ae6d7564693b5fb3872ec4e305
IsoonS <95904307+IsoonS@users.noreply.github.com>
2023-02-05 21:09:41 +0700

    edit add.js to can select sport

--- firebase_test/add.js
+++ firebase_test/add.js
@@ -1,8 +1,24 @@
-import {addNew } from './md.js';
+import { addNew } from './md.js';
 
 
-document.getElementById("new-bt").addEventListener("click", function() {
-    console.log("add new");
-    addNew("VB1", 0, 0, 0, 0, "volleyball", false)
+document.getElementById("new-bt").addEventListener("click", function () {
+  console.log("add new");
+  let e = document.getElementById("type-s");
+  let value = e.value;
+  let typeOfSport = e.options[e.selectedIndex].text;
+  addNew("", 0, 0, 0, 0, typeOfSport, false)
+  document.getElementById("new-bt").disabled = true;
+
+
+  var buttonEl = document.createElement("button");
+  buttonEl.innerText = "show new button"
+	buttonEl.onclick = () => {
+    document.getElementById("new-bt").disabled = false;
+  }
+  // document.getElementsByClassName("container")[0].appendChild(document.createElement("br"));
+  let k = document.getElementById("container-1");
+  // console.log(k);
+  k.appendChild(document.createElement("br"));
+  k.appendChild(buttonEl);
   // Your code here
 });

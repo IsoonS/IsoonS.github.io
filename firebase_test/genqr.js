5829e0dc58d7d1595770cc07410ef85b82f361c3
IsoonS <95904307+IsoonS@users.noreply.github.com>
2023-02-24 21:21:06 +0700

    update redirect method

--- firebase_test/genqr.js
+++ firebase_test/genqr.js
@@ -2,6 +2,8 @@
 
 // ./genqr.html?id=WeVcUEezIFNnnGsoNz82
 
+// https://stackoverflow.com/questions/814613/how-to-read-get-data-from-a-url-using-javascript
+
 const urlParams = new URLSearchParams(window.location.search);
 // const param1 = urlParams.get('param1'); // Returns 'value1'
 // const param2 = urlParams.get('param2'); // Returns 'value2'
@@ -16,5 +18,8 @@ console.log(id);
 
 let yourID = localStorage.getItem("yourID")
 
+
+console.log("before redirect");
 window.location.replace("https://isoons.github.io/firebase_test/view_by_id.html");
+
 
